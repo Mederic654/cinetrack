d730fcfa3ebf0c4f6ed6757311f98414a5e0b885
Mederic654 <medericl@outlook.com>
2026-06-11 12:45:41 +0200

    F13

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { App } from './app/app';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
 import { routes } from './app/app.routes';
+import { errorInterceptor } from './app/interceptors/error.interceptor';
 
 bootstrapApplication(App, {
   providers: [
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
     provideRouter(routes, withComponentInputBinding()),
   ],
 });
